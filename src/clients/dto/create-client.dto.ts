02130be53bf19d43e616d6336ac038997b916d64
Sebastian Giraldo <sebastian.giraldo69@eia.edu.co>
2026-02-21 11:01:59 -0500

    Se agregan validaciones en la creación de los clientes

--- src/clients/dto/create-client.dto.ts
+++ src/clients/dto/create-client.dto.ts
@@ -1,24 +1,33 @@
-import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+import { IsEmail, IsIn, IsNotEmpty, IsString, Matches, MinLength } from 'class-validator';
 
 export class CreateClientDto {
   @IsString()
-  @IsNotEmpty()
+  @IsNotEmpty({ message: 'El nombre es obligatorio' })
+  @MinLength(3, { message: 'El nombre debe tener al menos 3 caracteres' })
   fullName!: string;
 
-  @IsString()
-  @IsNotEmpty()
+   @IsString()
+  @IsNotEmpty({ message: 'El tipo de documento es obligatorio' })
+  @IsIn(['CC', 'CE', 'PAS'], {
+    message: 'Tipo de documento inválido',
+  })
   documentType!: string;
 
   @IsString()
-  @IsNotEmpty()
-  @MinLength(5)
+  @IsNotEmpty({ message: 'El número de documento es obligatorio' })
+  @MinLength(5, {
+    message: 'El documento debe tener al menos 5 dígitos',
+  })
   documentNumber!: string;
 
   @IsString()
-  @IsNotEmpty()
+  @IsNotEmpty({ message: 'El teléfono es obligatorio' })
+  @Matches(/^[0-9]{7,15}$/, {
+    message: 'Teléfono inválido',
+  })
   phone!: string;
 
-  @IsEmail()
-  @IsNotEmpty()
+  @IsEmail({}, { message: 'Email inválido' })
+  @IsNotEmpty({ message: 'El email es obligatorio' })
   email!: string;
 }
